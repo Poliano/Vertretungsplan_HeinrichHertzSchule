0408a452b032b636c6874138da10faf21704dd53
Poliano <ben.bertheau05@gmail.com>
2022-06-21 18:35:31 +0200

    forecast days

--- vertrungsplan.py
+++ vertrungsplan.py
@@ -4,17 +4,21 @@ import pandas as pd
 import lxml
 import numpy as np
 
-urllist = 3
-url = f"https://www.heinrich-hertz-schule-hamburg.de/vertretungsplan/subst_00{urllist}.htm"
-result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
-
-date = doc.div.string
-print(date)
-
-dfs = pd.read_html(url, index_col=False)
-df = dfs[1]
-
-substring = input("Eingabe: ")
+substring = input("Klasse: ")
 substring = substring.replace(" ", "")
-print(df[df.apply(lambda row: row.astype(str).str.contains(substring, case=False).any(), axis=1)])
+days = 3
+
+for urllist in range(days): 
+    urllist = urllist + 1
+    url = f"https://www.heinrich-hertz-schule-hamburg.de/vertretungsplan/subst_00{urllist}.htm"
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "html.parser")
+
+    date = doc.div.string
+    print(date)
+
+    dfs = pd.read_html(url, index_col=False)
+    df = dfs[1]
+    df = df[df.apply(lambda row: row.astype(str).str.contains(substring, case=False).any(), axis=1)]
+
+    print(df)
