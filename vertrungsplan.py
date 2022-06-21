ebcfd8af60d40e0d57b65b4da28688f19b712dfe
Poliano <ben.bertheau05@gmail.com>
2022-06-21 20:49:13 +0200

    Big change to search and added menu

--- vertrungsplan.py
+++ vertrungsplan.py
@@ -1,10 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+days = 3
+dropindex = "n"
 
 substring = input("Klasse: ")
+if str(substring).lower() == "hilfe" or str(substring).lower() == "help":
+    print(
+    '''Einfach den Namen der Klasse Eingeben oder sie Stufen Bezeichnung:
+    VS für Vorstufe oder 5 für alle Klassen aus dem Jahrgang 5
+    5a oder Med1 für die Klasse 5a oder das Medien Profil 1
+    Wenn du ohne eingabe Enter drückst bekommst du alle Klasse
+    Gebe Einstellungen oder Settings ein um in die Einstellungen zu kommen
+    ''')
+elif substring == "einstellungen" or str(substring).lower() == "settings":
+    days = int(input(f"({days}) Vorausschaunde Tage: "))
+    if days > 3 or days < 0 or days == "":
+        print("Days Fehler Zahl muss zwischen 0-3 sein")
+        days = 3
+    dropindex = str(input(f"({dropindex}) Index Neubeginnen: "))
+    if dropindex.lower() != "y" and dropindex.lower() != "n":
+        print("Falsche Eingabe Entweder Y oder N")
+        dropindex = "n"
+    
 substring = substring.replace(" ", "")
-days = 3
 
 for urllist in range(days): 
     urllist = urllist + 1
@@ -17,7 +36,10 @@ for urllist in range(days):
 
     dfs = pd.read_html(url, index_col=False)
     df = dfs[1]
-    df = df[df.apply(lambda row: row.astype(str).str.contains(substring, case=False).any(), axis=1)]
+    df = df[df['Klasse(n)'].str.contains(substring, case=False, na=False)]
+
+    if dropindex == "y":
+        df = df.reset_index(drop=True)
 
     if "Empty DataFrame" in str(df):
         print("Keine Vertretung")
